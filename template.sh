0505b592e47833cc5827a5c2bbf48e655da4e770
Gopal Dewoolkar <gopald1818@gmail.com>
2025-09-27 19:53:32 +0530

    Folder structure added.

---
+++ template.sh
@@ -0,0 +1,13 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch research/trials.ipynb
+touch setup.py
+touch app.py
