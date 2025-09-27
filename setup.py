7ec30104698b735f0f73013e9d0024de9da04f5d
Gopal Dewoolkar <gopald1818@gmail.com>
2025-09-28 00:08:24 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_Chatbot",
+    version="0.1.0",
+    author="Gopal Dewoolkar",
+    author_email="gopald1818@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
